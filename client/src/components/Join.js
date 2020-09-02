8f2fbbc79e60d45b62a7eaf7cd591fb559d7a1a6
mcmicko <mcmzrc@gmail.com>
2020-09-02 17:51:36 +0200

    Imp Socket in Index.js, add Router.js & create form Join.js

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Linl } from "react-router-dom";
 
 function Join() {
-  return <div>JOIN</div>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            type="text"
+            className="joinInput"
+            placeholder="Name"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="joinInput mt-2"
+            placeholder="Room"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            sign in
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Join;
